9c1f69f95f2c5ee51e10c3e9949b2395aa733059
Mateus Melo do Nascimento <mateus.networking@gmail.com>
2019-05-22 12:35:30 -0300

    First commit

---
+++ MiraclePill/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Mateus on 21/05/19.
+//  Copyright © 2019 Mateus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
