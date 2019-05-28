745f2fc00a73727a54c6a922ed4ae3ca39e9f7c3
Mateus Melo do Nascimento <mateus.networking@gmail.com>
2019-05-28 10:55:15 -0300

    added scroll view and text field delegates

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -8,9 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: Outlets reference
+    @IBOutlet weak var textFieldName: UITextField!
+    @IBOutlet weak var textFieldAddress: UITextField!
+    @IBOutlet weak var textFieldZipCode: UITextField!
+    @IBOutlet weak var textFieldCity: UITextField!
+    @IBOutlet weak var textFieldState: UITextField!
+    @IBOutlet weak var textFieldCountry: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Set delegate to textFields
+        textFieldName.delegate = self
+        textFieldAddress.delegate = self
+        textFieldZipCode.delegate = self
+        textFieldCity.delegate = self
+        textFieldState.delegate = self
+        textFieldCountry.delegate = self
+    }
+    
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+            case textFieldName:
+                textFieldAddress.becomeFirstResponder()
+            case textFieldAddress:
+                textFieldZipCode.becomeFirstResponder()
+            case textFieldZipCode:
+                textFieldCity.becomeFirstResponder()
+            case textFieldCity:
+                textFieldState.becomeFirstResponder()
+            case textFieldState:
+                textFieldCountry.becomeFirstResponder()
+            default:
+                textField.resignFirstResponder()
+        }
+        return false
     }
 }
