9b7ef7767090c19b2b5a6514e834eb6428f39d1b
Mateus <mateus@MacBook-Pro-de-Mateus.local>
2019-06-02 17:33:51 -0300

    Removed unused code

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -8,17 +8,6 @@
 
 import UIKit
 
-extension UITextField {
-    func setBottomBorderOnlyWith(color: CGColor) {
-        self.borderStyle = .none
-        self.layer.masksToBounds = false
-        self.layer.shadowColor = color
-        self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
-        self.layer.shadowOpacity = 1.0
-        self.layer.shadowRadius = 0.0
-    }
-}
-
 class ViewController: UIViewController, UITextFieldDelegate {
     
     // MARK: Outlets reference
